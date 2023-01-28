9e79eef934bb3c38feb2f9ec4d797dee8fba3491
Gab <gabbagen20@gmail.com>
2023-01-28 16:20:49 +0300

    fixed controllers

--- D2SProtrackerParser/src/main/java/d2s/protracker/controllers/ProTrackerParserController.java
+++ D2SProtrackerParser/src/main/java/d2s/protracker/controllers/ProTrackerParserController.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import d2s.protracker.ProTrackerMatchSelector;
@@ -18,14 +20,15 @@ public class ProTrackerParserController {
 	@Autowired
 	ProTrackerMatchSelector selector;
 	
-	@GetMapping("/d2sprotracker")
+	@GetMapping("/")
 	public String defaultAnswer() {
 		return "hi, I can parse the www.dota2protracker.com and return a list of IDs of recently played pro pubs";
 	}
 	
-	@GetMapping("/d2sprotracker/parse")
-	public ArrayList<Long> returnRecentMatchesIDs() {
-		return selector.getRecentMatches();
+	@GetMapping("/parse")
+	public ResponseEntity<ArrayList<Long>> returnRecentMatchesIDs() {
+		var matches = selector.getRecentMatches();
+		return ResponseEntity.ok().body(matches);
 	}
 	
 }
